7e09a405ed4bac3be52302f8cb8c411d92b54937
沖山 <okiyama1609@outlook.jp>
2022-11-23 01:00:13 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -54,6 +54,290 @@ void Player::Update(Camera viewProjection)
 {
 
 	moveVec = { 0,0,0 };
+	
+	Vector3 Flont /*= camera->getForwardVec()*/;
+	Vector3 KyozouFlont = Flont;
+	Flont.y = 0;
+
+	
+
+	bullets_.remove_if([](std::unique_ptr<PlayerBullet>& bullet)
+		{
+			return bullet->IsDead();
+		});
+
+	if (Input::GetInstance()->GetJoystickState(0, joystate))
+	{
+		moveVec.x += (float)joystate.Gamepad.sThumbLX / SHRT_MAX;
+		moveVec.z += (float)joystate.Gamepad.sThumbLY / SHRT_MAX;
+	}
+
+
+	if (input_->PushKey(DIK_UP))
+	{
+		moveVec.z = 1;
+	}
+	if (input_->PushKey(DIK_DOWN))
+	{
+		moveVec.z = -1;
+	}
+	if (input_->PushKey(DIK_RIGHT))
+	{
+		moveVec.x = 1;
+	}
+	if (input_->PushKey(DIK_LEFT))
+	{
+		moveVec.x = -1;
+	}
+
+	//ˆÚ“®
+
+
+	float p_pos = atan2(moveVec.x, moveVec.z);
+	float c_vec = atan2(Flont.x, Flont.z);
+
+	worldTransform_.rotation_.y = (p_pos + c_vec);
+
+	Vector3 mae = { 0,0,1.0f };
+
+	mae = worldTransform_.matWorld_.VectorMat(mae, worldTransform_.matWorld_);
+
+	mae.normalize();
+
+	if (!hopper_dash && !cooldown)
+	{
+		if (moveVec.x != 0 || moveVec.z != 0)
+		{
+
+			worldTransform_.translation_ += mae * move_speed;
+		}
+	}
+
+	EnemyArrow();
+
+	if (LockOn())
+	{
+		lockmove = true;
+	}
+	else
+	{
+		lockmove = false;
+		timer = 0;
+	}
+
+	if (lockmove)
+	{
+		timer++;
+
+		if (timer >= bitmovetimer)
+		{
+			timer = bitmovetimer;
+		}
+
+
+	}
+
+	if (LockOn())
+	{
+
+
+		for (size_t i = 0; i < gunbitnum; i++)
+		{
+			Vector3 PtoB = BitVec - target[i].translation_;
+			Vector3 kari = PtoB;
+			kari.y = 0;
+			float kariy = -PtoB.y;
+			PtoB.normalize();
+			float gomi = atan2(PtoB.x, PtoB.z);
+			target[i].rotation_.y = easeOutSine(target[i].rotation_.y, gomi, timer / bitmovetimer);
+			gomi = atan2(kariy, kari.length());
+			kyozou.rotation_.y = gomi;
+			target[i].rotation_.x = easeOutSine(target[i].rotation_.x, gomi, timer / bitmovetimer);
+			kyozou.rotation_.x = gomi;
+		}
+		Vector3 KtoB = BitVec - worldTransform_.translation_;
+		Vector3 Kkari = KtoB;
+		Kkari.y = 0;
+		float Kkariy = -KtoB.y;
+		KtoB.normalize();
+		float gomi = atan2(KtoB.x, KtoB.z);
+		kyozou.rotation_.y = gomi;
+		gomi = atan2(Kkariy, Kkari.length());
+		kyozou.rotation_.x = gomi;
+
+		for (size_t i = 0; i < gunbitnum; i++)
+		{
+			Vector3 bitpos = nannka[i];
+			bitpos = kyozou.matWorld_.VectorMat(bitpos, kyozou.matWorld_);
+			bitpos.normalize();
+			target[i].translation_ = easeOutSineVec3(target[i].translation_, worldTransform_.translation_ + (bitpos * 2.0f), timer / bitmovetimer);
+		}
+
+
+	}
+	else
+	{
+		for (size_t i = 0; i < gunbitnum; i++)
+		{
+			Vector3 bitpos = nannka[i];
+			bitpos = worldTransform_.matWorld_.VectorMat(bitpos, worldTransform_.matWorld_);
+			bitpos.normalize();
+			target[i].translation_ = worldTransform_.translation_ + (bitpos * 2.0f);
+		}
+
+		for (size_t i = 0; i < gunbitnum; i++)
+		{
+
+			target[i].rotation_.y = worldTransform_.rotation_.y;
+			target[i].rotation_.x = worldTransform_.rotation_.x;
+		}
+	}
+
+
+
+
+
+	old_B_bottan = B_bottan;
+
+	if (joystate.Gamepad.wButtons & XINPUT_GAMEPAD_A)
+	{
+		B_bottan = true;
+	}
+	else
+	{
+		B_bottan = false;
+	}
+
+
+	if ((input_->TriggerKey(DIK_Z) || (B_bottan && !old_B_bottan)) && !cooldown)
+	{
+		hopper_dash = true;
+
+		cooltime = 60;
+
+		hoppertimer = 0;
+
+		dash_vec = mae;
+
+		dash_vec.normalize();
+
+	}
+	if (hopper_dash)
+	{
+		if (!cooldown)
+		{
+			worldTransform_.translation_ = easeOutSineVec3(worldTransform_.translation_, worldTransform_.translation_ + dash_vec * hopper_speed, hoppertimer / hoppertime);
+
+		}
+		hoppertimer++;
+
+
+
+
+		if (hoppertimer >= hoppertime)
+		{
+			hoppertimer = hoppertime;
+			cooltime--;
+			cooldown = true;
+
+		}
+		if (cooltime <= 0)
+		{
+			hopper_dash = false;
+			cooldown = false;
+		}
+
+	}
+
+
+	worldTransform_.translation_.y -= 0.1f;
+	moveVec.y = -1;
+
+
+
+
+	if ((input_->PushKey(DIK_SPACE) || (joystate.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER)) && hoppertimer >= 0)
+	{
+		if (!LockOn())
+		{
+			Vector3 kari2 = KyozouFlont;
+			kari2.y = 0;
+			float kariy2 = -KyozouFlont.y;
+
+			float gomi = atan2(Flont.x, Flont.z);
+			for (size_t i = 0; i < gunbitnum; i++)
+			{
+				target[i].rotation_.y = easeOutSine(target[i].rotation_.y, gomi, NormalTimer / BitNormalAttakTime);
+			}
+			kyozou.rotation_.y = gomi;
+			gomi = atan2(kariy2, kari2.length());
+			for (size_t i = 0; i < gunbitnum; i++)
+			{
+				target[i].rotation_.x = easeOutSine(target[i].rotation_.x, gomi, NormalTimer / BitNormalAttakTime);
+			}
+			kyozou.rotation_.x = gomi;
+
+			for (size_t i = 0; i < gunbitnum; i++)
+			{
+				Vector3 bitpos = nannka[i];
+				bitpos = kyozou.matWorld_.VectorMat(bitpos, kyozou.matWorld_);
+				bitpos.normalize();
+				target[i].translation_ = easeOutSineVec3(target[i].translation_, worldTransform_.translation_ + (bitpos * 2.0f), NormalTimer / BitNormalAttakTime);
+			}
+
+
+			NormalTimer++;
+
+			if (NormalTimer >= BitNormalAttakTime)
+			{
+				NormalTimer = BitNormalAttakTime;
+			}
+		}
+		else
+		{
+			NormalTimer = 0;
+		}
+
+
+		if (latetime <= 0)
+		{
+			Attack(camera->getForwardVec());
+			latetime = firelate;
+		}
+		latetime--;
+
+	}
+	else
+	{
+		NormalTimer = 0;
+	}
+
+
+	for (std::unique_ptr<PlayerBullet>& bullet : bullets_)
+	{
+		bullet->Update();
+	}
+
+	for (size_t i = 0; i < gunbitnum; i++)
+	{
+		if (i == 0 || i == 2)
+		{
+			target[i].rotation_.z += 0.05f;
+		}
+		else
+		{
+			target[i].rotation_.z -= 0.05f;
+		}
+		target[i].matWorldGeneration();
+	}
+
+	kyozou.matWorldGeneration();
+
+	worldTransform_.matWorldGeneration();
+
+	//bosstarget->SetPosition(kasu(boss->getPos().translation_));
+
+
 
 
 }
--- Player.h
+++ Player.h
@@ -15,7 +15,50 @@ using namespace Input;
 
 class Player
 {
+	struct Vector3 : public XMFLOAT3 {
 
+		Vector3() = default;
+		Vector3(float x, float y, float z)
+		{
+			this->x = x; this->y = y; this->z = z;
+		}
+		//コンストラクタ
+		Vector3(const XMVECTOR& other) :XMFLOAT3() {
+			XMVECTOR temp = other;
+			XMStoreFloat3(this, temp);
+		}
+
+		inline BOOL operator == (const Vector3& r) const { return x == r.x && y == r.y && z == r.z; }
+		inline BOOL operator != (const Vector3& r) const { return x != r.x || y != r.y || z != r.z; }
+		inline XMVECTOR operator *(const float r) const { return Vector3(x * r, y * r, z * r); }
+		inline XMVECTOR operator /(const float r) const { return Vector3(x / r, y / r, z / r); }
+
+		// ベクトルの内積
+		float VDot(Vector3 In) { return x * In.x + y * In.y + z * In.z; }
+		// ベクトルの外積
+		Vector3 VCross(Vector3 In) { return Vector3(y * In.z - z * In.y, z * In.x - x * In.z, x * In.y - y * In.x); }
+		// ベクトルのスケーリング
+		Vector3 VScale(float Scale) { Vector3 Result = { x * Scale, y * Scale, z * Scale }; return Result; }
+
+		//代入
+		Vector3& operator=(const XMVECTOR& other) {
+			XMVECTOR temp = other;
+			XMStoreFloat3(this, temp);
+			return *this;
+		}
+		//キャスト
+		operator XMVECTOR() const {
+			return XMLoadFloat3(this);
+		}
+		//長さ
+		float Length() const {
+			return (static_cast<Vector3>(XMVector3Length(XMVECTOR(*this)))).x;
+		}
+		//正規化
+		void normalize() {
+			*this = XMVector3Normalize(XMVECTOR(*this));
+		}
+	};
 
 public:
 	Player();
